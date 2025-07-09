eaa47f03eae86f049e307d457c49420f15b3aa9b
Alejandro Giraldo Serna <giraldosernaalejandro16@gmail.com>
2025-07-09 17:04:46 -0500

    Form login was completed

--- src/app/form-login/form-login.ts
+++ src/app/form-login/form-login.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-form-login',
-  imports: [],
-  templateUrl: './form-login.html',
-  styleUrl: './form-login.scss'
+  selector: "app-form-login",
+  imports: [ReactiveFormsModule],
+  templateUrl: "./form-login.html",
+  styleUrl: "./form-login.scss",
 })
-export class FormLogin {
+export class FormLogin implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl("", Validators.required),
+      password: new FormControl("", Validators.required),
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log("Formulario enviado", this.loginForm.value);
+    } else {
+      console.log("Formulario inválido");
+      this.loginForm.markAllAsTouched(); // Marca todos los campos como tocados para mostrar los errores
+    }
+  }
 }
